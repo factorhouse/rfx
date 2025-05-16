43cafd6f57d5e7e83b6492331b8e4b83c1f8e494
Thomas Crowley <tom@factorhouse.io>
2025-05-17 09:17:27 +1000

    0.1.15

--- modules/re-frame-bridge/project.clj
+++ modules/re-frame-bridge/project.clj
@@ -1,4 +1,4 @@
-(defproject io.factorhouse/re-frame-bridge "0.1.14"
+(defproject io.factorhouse/re-frame-bridge "0.1.15"
   :description "An implementation of re-frame built for modern React (compatibility api)"
   :url "http://github.com/factorhouse/rfx"
   :license {:name         "Apache-2.0 License"
@@ -6,7 +6,7 @@
             :distribution :repo
             :comments     "same as Kafka"}
   :dependencies [[org.clojure/clojure "1.12.0" :scope "provided"]
-                 [io.factorhouse/rfx "0.1.13"]]
+                 [io.factorhouse/rfx "0.1.15"]]
   :source-paths ["src"]
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :username :env/CLOJARS_USERNAME
