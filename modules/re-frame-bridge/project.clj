e895998b724d6aa53eecf61042d8b5d12c699c91
Thomas Crowley <tom@factorhouse.io>
2025-07-03 20:37:05 +1000

    Bump rfx in re-frame-bridge

--- modules/re-frame-bridge/project.clj
+++ modules/re-frame-bridge/project.clj
@@ -6,7 +6,7 @@
             :distribution :repo
             :comments     "same as Kafka"}
   :dependencies [[org.clojure/clojure "1.12.0" :scope "provided"]
-                 [io.factorhouse/rfx "0.1.17"]]
+                 [io.factorhouse/rfx "0.1.18"]]
   :source-paths ["src"]
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :username :env/CLOJARS_USERNAME
